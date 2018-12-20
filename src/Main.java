3e05a47c80fdf2436441f2c07a40ead4ece0009d
GefoS <embers989@gmail.com>
2018-12-20 19:55:44 +0300

    lol

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+//import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ArrayList <Integer> list = new ArrayList <> ();
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\GefoS\\IdeaProjects\\untitled\\src\\test.txt"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            list.add (Integer.valueOf(line));
+        }
+        System.out.println("Исходный массив имеет вид:");
+        for (Integer i : list) {
+            System.out.println (i);
+        }
+        list.sort((a, b) -> a.compareTo(b));
+        System.out.println("Отсортированный массив имеет вид:");
+        for (Integer i : list) {
+            System.out.println (i);
+        }
+        br.close();
+        //list.sort();
+    }
+}
